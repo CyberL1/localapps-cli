8b529d4eab432ceb83db1c15f3b4919032b289f8
CyberL1 <mateusz.hf18@gmail.com>
2025-10-11 22:57:58 +0200

    refactor(types): remove not needed `GithubFile` type

--- src/types/GithubRelease.go
+++ src/types/GithubRelease.go
@@ -4,9 +4,3 @@ type GithubRelease struct {
 	TagName    string `json:"tag_name"`
 	Prerelease bool
 }
-
-type GithubFile struct {
-	Name        string `json:"name"`
-	Path        string `json:"path"`
-	DownloadUrl string `json:"download_url"`
-}
