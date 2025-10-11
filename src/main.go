a532680538e094640c68de3aefea0c7d81547933
CyberL1 <mateusz.hf18@gmail.com>
2025-10-11 09:26:18 +0200

    fix(src): create config directory if does not exist

--- src/main.go
+++ src/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"localapps-cli/cmd"
+	"localapps-cli/constants"
 	"localapps-cli/utils"
+	"os"
 )
 
 func main() {
+	// Check for all required resources
+	if _, err := os.Stat(constants.LocalappsDir); os.IsNotExist(err) {
+		if err := os.Mkdir(constants.LocalappsDir, 0755); err != nil {
+			fmt.Printf("Failed to create %s directory: %s\n", constants.LocalappsDir, err)
+			return
+		}
+	}
+
 	utils.UpdateCliConfigCache()
 	cmd.Execute()
 }
