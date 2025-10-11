041170f6d1210b0a4323af6cef3fc7e93813e234
CyberL1 <mateusz.hf18@gmail.com>
2025-10-11 22:57:23 +0200

    refactor(src): remove server-related things

--- src/types/Api.go
+++ src/types/Api.go
@@ -5,16 +5,3 @@ type ApiError struct {
 	Message string `json:"message"`
 	Error   error  `json:"error,omitempty"`
 }
-
-type ApiAppResponse struct {
-	Id          int64             `json:"id"`
-	AppId       string            `json:"appId"`
-	Name        string            `json:"name"`
-	Icon        string            `json:"icon"`
-	InstalledAt string            `json:"installedAt"`
-	Parts       map[string]string `json:"parts"`
-}
-
-type ApiAppInstallRequestBody struct {
-	File string `json:"file"`
-}
--- src/types/Config.go
+++ src/types/Config.go
@@ -1,10 +1,5 @@
 package types
 
-type ServerConfig struct {
-	AccessUrl string `default:"\"http://localhost:8080\""`
-	ApiKey    string `default:"\"\""`
-}
-
 type CliConfig struct {
 	Server CliConfigServer `json:"server"`
 }
