6b3a80353332a6c5a7e999c1d5f028ec90a94125
CyberL1 <cyber.hf18@gmail.com>
2026-01-03 11:05:40 +0100

    feat(cmd): push app images on deploy

--- src/cmd/deploy.go
+++ src/cmd/deploy.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -10,8 +11,15 @@ import (
 	"localapps-cli/utils"
 	"mime/multipart"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
 )
@@ -35,6 +43,14 @@ var deployCmd = &cobra.Command{
 }
 
 func uploadApp(appFilePath string, update bool) error {
+	cli, _ := client.NewClientWithOpts(client.FromEnv)
+
+	_, err := cli.Ping(context.Background())
+	if err != nil {
+		fmt.Println("Failed to connect to Docker daemon. Is it running?")
+		return err
+	}
+
 	appFile, err := os.Open(appFilePath)
 	if err != nil {
 		return fmt.Errorf("error opening file: %s", err)
@@ -96,7 +112,82 @@ func uploadApp(appFilePath string, update bool) error {
 		return fmt.Errorf("error closing writer: %s", err)
 	}
 
-	req, err := http.NewRequest("POST", utils.CliConfig.Server.Url+"/api/apps/", &requestBody)
+	fmt.Println("Building " + appInfo.Name)
+
+	var appId string
+	if appInfo.Id != "" {
+		appId = appInfo.Id
+	} else {
+		appId = strings.ToLower(strings.ReplaceAll(appInfo.Name, " ", "-"))
+	}
+
+	for partName, part := range appInfo.Parts {
+		buildCmd := exec.Command("docker", "build", "-t", "localapps/apps/"+appId+"/"+partName, part.Src)
+
+		buildCmd.Stdout = os.Stdout
+		buildCmd.Stderr = os.Stderr
+
+		buildCmd.Run()
+	}
+
+	fmt.Println("Deploying app to the server")
+
+	openRegistryReq, err := http.NewRequest("GET", utils.CliConfig.Server.Url+"/api/registry", nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %s", err)
+	}
+
+	openRegistryReq.Header.Set("Authorization", utils.CliConfig.Server.ApiKey)
+
+	openRegistryResp, err := http.DefaultClient.Do(openRegistryReq)
+	if err != nil {
+		return fmt.Errorf("error sending request: %s", err)
+	}
+	defer openRegistryResp.Body.Close()
+
+	body, err := io.ReadAll(openRegistryResp.Body)
+	if err != nil {
+		return fmt.Errorf("error reading response body: %s", err)
+	}
+
+	var openRegistrybodyJson struct {
+		Port string `json:"port"`
+	}
+	json.Unmarshal(body, &openRegistrybodyJson)
+
+	serverUrlParsed, _ := url.Parse(utils.CliConfig.Server.Url)
+	registryTarget, _ := url.Parse(openRegistryReq.URL.Scheme + "://" + serverUrlParsed.Hostname() + ":" + openRegistrybodyJson.Port)
+
+	proxy := httputil.NewSingleHostReverseProxy(registryTarget)
+	proxy.FlushInterval = -1
+
+	proxyPort, _ := utils.GetFreePort()
+	proxyPortString := strconv.Itoa(proxyPort)
+
+	server := &http.Server{
+		Addr:    "127.0.0.1:" + proxyPortString,
+		Handler: proxy,
+	}
+
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	for partName := range appInfo.Parts {
+		cli.ImageTag(context.Background(), "localapps/apps/"+appId+"/"+partName, "localhost:"+proxyPortString+"/localapps/apps/"+appId+"/"+partName)
+		pushResp, err := cli.ImagePush(context.Background(), "localhost:"+proxyPortString+"/localapps/apps/"+appId+"/"+partName, image.PushOptions{})
+
+		if err != nil {
+			return fmt.Errorf("error pushing image: %w", err)
+		}
+		defer pushResp.Close()
+
+		if _, err := io.Copy(os.Stdout, pushResp); err != nil {
+			return fmt.Errorf("error streaming push output: %w", err)
+		}
+	}
+
+	req, err := http.NewRequest("POST", utils.CliConfig.Server.Url+"/api/apps", &requestBody)
 	if err != nil {
 		return fmt.Errorf("error creating request: %s", err)
 	}
@@ -110,7 +201,7 @@ func uploadApp(appFilePath string, update bool) error {
 	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	body, err = io.ReadAll(resp.Body)
 	if err != nil {
 		return fmt.Errorf("error reading response body: %s", err)
 	}
@@ -122,11 +213,23 @@ func uploadApp(appFilePath string, update bool) error {
 		return uploadApp(appFilePath, true)
 	}
 
+	closeRegistryReq, err := http.NewRequest("DELETE", utils.CliConfig.Server.Url+"/api/registry", nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %s", err)
+	}
+
+	closeRegistryReq.Header.Set("Authorization", utils.CliConfig.Server.ApiKey)
+
+	_, err = http.DefaultClient.Do(closeRegistryReq)
+	if err != nil {
+		return fmt.Errorf("error sending request: %s", err)
+	}
+
 	if resp.StatusCode != http.StatusNoContent {
 		fmt.Printf("[Error -> %s] %s\n\n", bodyJson.Code, bodyJson.Message)
 		fmt.Println(bodyJson.Error.Error())
 	} else {
-		fmt.Println("App deployed. Find it on the server:", utils.CliConfig.Server.Url)
+		fmt.Println("\nApp deployed. Find it on the server:", utils.CliConfig.Server.Url)
 	}
 
 	return nil
